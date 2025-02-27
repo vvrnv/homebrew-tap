262642d240885db7174c637576bdabc2d323c14a
vvrnv <valeravoronov96@gmail.com>
2025-02-27 16:14:38 +0300

    Brew formula update for kctxsync version v0.4.0

--- Formula/kctxsync.rb
+++ Formula/kctxsync.rb
@@ -5,21 +5,21 @@
 class Kctxsync < Formula
   desc "A command-line tool to sync certificate and key data from a remote Kubernetes cluster's kubeconfig to your local kubeconfig."
   homepage "https://vvrnv.dev"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
-    on_intel do
-      url "https://github.com/vvrnv/kctxsync/releases/download/v0.3.0/kctxsync_0.3.0_darwin_amd64.tar.gz"
-      sha256 "9e7e6e4b7d4412f6fcb0495ca07e8b94318a4b4495302a06926d5c10933e5f3e"
+    if Hardware::CPU.intel?
+      url "https://github.com/vvrnv/kctxsync/releases/download/v0.4.0/kctxsync_0.4.0_darwin_amd64.tar.gz"
+      sha256 "a5aa992958c8997a652a0af01e0c701275a1e2b9eb68a8718db654c0498f3cce"
 
       def install
         bin.install "kctxsync"
       end
     end
-    on_arm do
-      url "https://github.com/vvrnv/kctxsync/releases/download/v0.3.0/kctxsync_0.3.0_darwin_arm64.tar.gz"
-      sha256 "36cc738c632357e362687ca4ce0238479fd50d6816c96d90e7a6d3631f6972bf"
+    if Hardware::CPU.arm?
+      url "https://github.com/vvrnv/kctxsync/releases/download/v0.4.0/kctxsync_0.4.0_darwin_arm64.tar.gz"
+      sha256 "f3256fd51496093405c75c1ab84866d7810216bf7623d3102d8c0a51ea79ca8b"
 
       def install
         bin.install "kctxsync"
@@ -28,20 +28,20 @@ class Kctxsync < Formula
   end
 
   on_linux do
-    on_intel do
+    if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/vvrnv/kctxsync/releases/download/v0.3.0/kctxsync_0.3.0_linux_amd64.tar.gz"
-        sha256 "6bfbe02b4c73d559a2c6243c482b39d32e49901135dd9078b5f1299e36855690"
+        url "https://github.com/vvrnv/kctxsync/releases/download/v0.4.0/kctxsync_0.4.0_linux_amd64.tar.gz"
+        sha256 "f2d0ccf979113a61db3a3ab3e09d854045fe44527bcc33ee938d8250ebaafc95"
 
         def install
           bin.install "kctxsync"
         end
       end
     end
-    on_arm do
+    if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/vvrnv/kctxsync/releases/download/v0.3.0/kctxsync_0.3.0_linux_arm64.tar.gz"
-        sha256 "3fd05f5d67628516cd02009983d79298ad1ec0a32109bd87e2132d12ddfdaf93"
+        url "https://github.com/vvrnv/kctxsync/releases/download/v0.4.0/kctxsync_0.4.0_linux_arm64.tar.gz"
+        sha256 "2cfe7af6a91a641a3a5d2e33b5b229543fb89dd5aa19686eb4f1f6ed060114a1"
 
         def install
           bin.install "kctxsync"
